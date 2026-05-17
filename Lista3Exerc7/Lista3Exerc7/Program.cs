8bf1e9ec0de3057e4c5dfe14289e0d09714cd225
Guimascena <gmascena.odm@gmail.com>
2026-05-17 10:53:21 -0300

    Add files via upload

---
+++ Lista3Exerc7/Lista3Exerc7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Exerc7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numero = 1; numero <= 20; numero++)
+            {
+                Console.Clear();
+
+                Console.WriteLine($"TABUADA DO {numero}\n");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{numero} x {i} = {numero * i}");
+                }
+
+                Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
