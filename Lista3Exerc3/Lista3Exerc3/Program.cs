c027c413a9ecb8c376b8ef007d3cb3881917d03c
Guimascena <gmascena.odm@gmail.com>
2026-05-17 10:29:43 -0300

    Add files via upload

---
+++ Lista3Exerc3/Lista3Exerc3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Exerc3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+
+            do
+            {
+                Console.WriteLine("Digite o sexo (F/M):");
+                sexo = char.Parse(Console.ReadLine());
+
+                if (sexo != 'F' && sexo != 'f' &&
+                    sexo != 'M' && sexo != 'm')
+                {
+                    Console.WriteLine("Sexo inválido!");
+                }
+
+            } while (sexo != 'F' && sexo != 'f' &&
+                     sexo != 'M' && sexo != 'm');
+
+            Console.WriteLine("Sexo aceito.");
+        }
+    }
+}
