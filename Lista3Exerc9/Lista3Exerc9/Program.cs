764c5219cdff0e731d4a001df1ab121788827958
Guimascena <gmascena.odm@gmail.com>
2026-05-17 15:27:38 -0300

    Add files via upload

---
+++ Lista3Exerc9/Lista3Exerc9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Exerc9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 1, c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for (int i = 3; i <= 30; i++)
+            {
+                c = a + b;
+
+                Console.WriteLine(c);
+
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
