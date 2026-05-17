4dafe99b1d4043b519aaebef7a17c48e85d93538
Guimascena <gmascena.odm@gmail.com>
2026-05-17 10:24:28 -0300

    Add files via upload

---
+++ Lista3Exerc2/Lista3Exerc2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Exerc2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.WriteLine("Digite o primeiro valor:");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            b = int.Parse(Console.ReadLine());
+
+            while (b <= a)
+            {
+                Console.WriteLine("O segundo valor deve ser maior que o primeiro.");
+                Console.WriteLine("Digite novamente o segundo valor:");
+                b = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Primeiro valor: {a}");
+            Console.WriteLine($"Segundo valor: {b}");
+        }
+    }
+}
