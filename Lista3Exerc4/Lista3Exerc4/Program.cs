47c9a8e7c989729b91b4f2bc72d3465f569d5255
Guimascena <gmascena.odm@gmail.com>
2026-05-17 10:34:53 -0300

    Add files via upload

---
+++ Lista3Exerc4/Lista3Exerc4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Exerc4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+
+        }
+    }
+}
