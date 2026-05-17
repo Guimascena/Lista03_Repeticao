033b875f61a9adea3a2889bad59e734eb769233a
Guimascena <gmascena.odm@gmail.com>
2026-05-17 16:14:55 -0300

    Add files via upload

---
+++ Lista3Exerc13/Lista3Exerc13/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Exerc13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char resposta;
+
+            do
+            {
+                int n;
+                int fatorial = 1;
+
+                do
+                {
+                    Console.WriteLine("Digite um valor não negativo:");
+                    n = int.Parse(Console.ReadLine());
+
+                    if (n < 0)
+                    {
+                        Console.WriteLine("Valor inválido!");
+                    }
+
+                } while (n < 0);
+
+                for (int i = n; i >= 1; i--)
+                {
+                    fatorial *= i;
+                }
+
+                Console.WriteLine($"Fatorial de {n} = {fatorial}");
+
+                do
+                {
+                    Console.WriteLine("Deseja realizar outro cálculo? (S/N)");
+                    resposta = char.Parse(Console.ReadLine());
+
+                    if (resposta != 'S' && resposta != 's' &&
+                        resposta != 'N' && resposta != 'n')
+                    {
+                        Console.WriteLine("Resposta inválida!");
+                    }
+
+                } while (resposta != 'S' && resposta != 's' &&
+                         resposta != 'N' && resposta != 'n');
+
+            } while (resposta == 'S' || resposta == 's');
+        }
+    }
+}
