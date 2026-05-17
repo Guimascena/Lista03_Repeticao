a5dc882f59934dddd368cc08ddd22c1822a73068
Guimascena <gmascena.odm@gmail.com>
2026-05-17 15:36:38 -0300

    Add files via upload

---
+++ Lista3Exerc8/Lista3Exerc8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Exerc8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += i;
+            }
+
+            Console.WriteLine($"Soma dos números de 1 a 100: {soma}");
+        }
+    }
+}
