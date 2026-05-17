2745afa8f49cf85f8241c12c60899c154e97d1ed
Guimascena <gmascena.odm@gmail.com>
2026-05-17 10:05:40 -0300

    Add files via upload

---
+++ Lista3Exerc1/Lista3Exerc1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Exerc1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            do
+            {
+                Console.WriteLine("Digite um valor positivo:");
+                valor = int.Parse(Console.ReadLine());
+
+                if (valor <= 0)
+                {
+                    Console.WriteLine("Valor inválido!");
+                }
+
+            } while (valor <= 0);
+
+            Console.WriteLine($"Valor aceito: {valor}");
+        }
+    }
+}
